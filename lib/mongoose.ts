0939332755f0b3b256d0333ead7357395ecbaeed
luc-tuyishime <luctunechi45@gmail.com>
2023-09-25 15:42:31 +0200

    update mongo url in env

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -2,17 +2,13 @@ import mongoose from 'mongoose'
 
 let isConnected = false // Variable to check if mongoose is connected
 
-console.log('process ==>>>', process.env.MONGODB_URL)
-
 export const connectToDB = async () => {
 	mongoose.set('strictQuery', true)
 	// if (!process.env.MONGOBD_URL) return console.log('MONGODB_URL NOT FOUND')
 	// if (isConnected) return console.log('Already connected to MongoDB')
 
 	try {
-		await mongoose.connect(
-			'mongodb+srv://jeanluc:Mauricetteluc10!@lucas.vo2znkq.mongodb.net/?retryWrites=true&w=majority',
-		)
+		await mongoose.connect(process.env.MONGODB_URL)
 
 		isConnected = true
 
