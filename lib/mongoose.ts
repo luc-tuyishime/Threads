ddf56c072615cc9fcb94f4c291cd5185d3fc8cad
luc-tuyishime <luctunechi45@gmail.com>
2023-10-09 10:42:19 +0200

    update new changes

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,10 +4,6 @@ let isConnected = false // Variable to check if mongoose is connected
 
 export const connectToDB = async () => {
 	mongoose.set('strictQuery', true)
-	if (!process.env.MONGOBD_URL) return console.log('MONGODB_URL NOT FOUND')
-	if (isConnected) return console.log('Already connected to MongoDB')
-
-	console.log('process ==>>>', process.env.MONGODB_URL)
 
 	try {
 		// @ts-ignore
